fb84c75c1a0b004cc76ca05fb73b00518ead45db
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-06 22:30:29 +0100

    toFiexed method and display finalAmount

--- src/FinalAmount/index.js
+++ src/FinalAmount/index.js
@@ -1,12 +1,12 @@
 import "./style.css"
 
-const FinalAmount = ({firstAmount, typeCurrency, intoPLN}) => (
+const FinalAmount = ({firstAmount, typeCurrency, intoPLN, finalAmount}) => (
     <div>
     <p>
       <strong className="section__currencyField">
         {firstAmount == 0 ? "0.00" : firstAmount} {intoPLN ? typeCurrency : "PLN"}
         </strong> jest warte:
-      <strong className="section__resultField"> 0.00 {intoPLN ? "PLN" : typeCurrency}</strong>
+      <strong className="section__resultField">{finalAmount == 0 ? "0.00" : finalAmount.toFixed(2)} {intoPLN ? "PLN" : typeCurrency}</strong>
     </p>
     </div>
 );
