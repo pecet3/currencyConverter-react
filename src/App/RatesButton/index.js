ac701a57c9fda8c175f311c50952ee8f89e56303
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-10 01:47:08 +0100

    added logic for display

--- src/App/RatesButton/index.js
+++ src/App/RatesButton/index.js
@@ -1,11 +1,14 @@
 import "./style.css"
 
-const RatesButton = ({showRates, setShowRates}) => {
+const RatesButton = ({ showRates, setShowRates }) => {
     const onClickButton = () => {
         setShowRates(!showRates);
-      }
-      return (
-      <button onClick={onClickButton} className="ratesButton">Pokaż Kursy Walut</button>
-      )
+    }
+    return (
+        <button
+            onClick={onClickButton} className="ratesButton">
+            {showRates ? "Ukryj" : "Pokaż"} Kursy Walut
+        </button>
+    )
 }
 export default RatesButton;
