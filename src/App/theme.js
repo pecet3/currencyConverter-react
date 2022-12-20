214621c7b5bce2a8d53a2ebfa8b754d379c41aed
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-20 21:30:00 +0100

    added colors

---
+++ src/App/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        transparentWhite: "rgba(254, 246, 238, 0.589)",
+        transparentGrey: "rgba(196, 189, 182, 0.708)",
+    },
+    breakpoints: {
+        mobile: 768
+    },
+};
