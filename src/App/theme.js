7cd5c6c3151f5e007560abbb4d509a834507b928
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-23 00:15:35 +0100

    removed colors

--- src/App/theme.js
+++ src/App/theme.js
@@ -6,15 +6,12 @@ export const theme = {
         lessTransparentBleachWhite: "rgba(250, 235, 215, 0.913)",
         almond: "hsl(33, 82%, 87%)",
         darkAlmond: "hsl(34, 52%, 78%)",
-        moreDarkAlmond: "hsl(34, 52%, 75%)",
         blueGrey: "rgb(212, 212, 212)",
         darkBlueGrey: "rgb(192, 192, 192)",
         veryTransparentBlack: "rgba(0, 0, 0, 0.2)",
         lightGrey: "hsl(195, 8%, 81%)",
         transparentLightGrey: "rgba(226, 223, 223, 0.66)",
-        veryLightGrey: "hsl(195, 8%, 88%)",
         transparentAlmond: "rgba(241, 232, 188, 0.425)",
-        
     },
     breakpoints: {
         mobile: 768,
