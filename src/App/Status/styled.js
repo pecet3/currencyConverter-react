e6f84a9c6594d7910573434273659b4996624b94
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-26 22:02:49 +0100

    removed @miedia

--- src/App/Status/styled.js
+++ src/App/Status/styled.js
@@ -5,10 +5,6 @@ export const Container = styled.div`
     min-height: 425px;
     padding-top: 150px;
 
-    @media(max-width:${({theme})=>theme.breakpoints.mobile}px){
-            min-height: 473.5px;
-        }
-
     ${({ error })=> error && css`
         font-size: 18px;
         color: ${({theme})=>theme.colors.shiraz};
@@ -17,9 +13,6 @@ export const Container = styled.div`
         min-height: 0;
         padding-top: 0;
         padding: 76.5px 0;
-        @media(max-width:${({theme})=>theme.breakpoints.mobile}px){
-            padding:153px 0;
-        }
     `}
     
 `;
