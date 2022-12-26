0fe57af4d3561219594afd1a9ec95e2d136f81dd
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-26 22:00:17 +0100

    added disabled property for button

--- src/App/ButtonAndDate/index.js
+++ src/App/ButtonAndDate/index.js
@@ -2,8 +2,10 @@ import Clock from "./Clock";
 import { Container, Button } from './styled.js';
 import { ThemeProvider } from "styled-components"
 import { theme } from "../theme.js"
+import { useCurrencies } from "../useCurrencies";
 
 const ButtonAndDate = ({ showRates, setShowRates}) => {
+    const currencies = useCurrencies();
     const onClickButton = () => {
         setShowRates(!showRates);
     }
@@ -12,7 +14,13 @@ const ButtonAndDate = ({ showRates, setShowRates}) => {
         <ThemeProvider theme={theme}>
         <Container>
         <Button
-            onClick={onClickButton} className="buttonAndDate__button">
+            onClick={onClickButton}
+            className="buttonAndDate__button"
+            disabled={
+            currencies.status==="error"
+            || currencies.status==="loading"
+            ? true
+            : false}>
             {showRates ? "Ukryj" : "Pokaż"} Kursy Walut
         </Button>
         <Clock/>
--- src/App/Status/index.js
+++ src/App/Status/index.js
@@ -1,14 +1,14 @@
 import { Container } from "./styled.js";
 
 export const StatusLoading = ({ list }) => {
-        return <Container list={list}> Proszę czekać, strona się ładuje 😎. </Container>
+        return <Container list={list}> Proszę czekać, trwa ładowanie 😎. </Container>
 };
 
 export const StatusError = ({ list }) => {
         return (
             <Container error list={list}>
                 Ups... wystąpiło coś nie tak 😪. Proszę sprawdź swoje połączenie z internetem.
-                Jeśli jest prawidłowe, problem leży po naszej stronie
+                Jeśli jest prawidłowe, problem leży po naszej stronie.
             </Container>
         )
     };
